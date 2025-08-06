adf3b541caac8ad38f154559ce21c2a77a22c554
praise-18 <praise187006@gmail.com>
2025-08-06 21:31:31 +0530

    rotate_matrix.c

--- rotate_matrix.c
+++ rotate_matrix.c
@@ -1,4 +1,3 @@
-C:
 #include <stdio.h>
 int main() {
     int n;
@@ -28,18 +27,3 @@ int main() {
             printf("%d ", mat[i][j]);
     return 0;
 }
-Python:
-n = int(input())
-mat = [list(map(int, input().split())) for _ in range(n)]
-
-# Transpose
-for i in range(n):
-    for j in range(i, n):
-        mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
-
-# Reverse rows
-for row in mat:
-    row.reverse()
-
-for row in mat:
-    print(*row)
